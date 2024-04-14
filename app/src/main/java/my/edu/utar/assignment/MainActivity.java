4515f5342d9d67c7faa1f3e57ecb95e83ce54789
Hauooo <howhau@1utar.my>
2024-04-14 20:18:35 +0800

    0.2

--- app/src/main/java/my/edu/utar/assignment/MainActivity.java
+++ app/src/main/java/my/edu/utar/assignment/MainActivity.java
@@ -1,15 +1,14 @@
 package my.edu.utar.assignment;
 
-import my.edu.utar.assignment.R;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,52 +17,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button normalTranslationButton = findViewById(R.id.action_normal_translation);
+        normalTranslationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent normal = new Intent(MainActivity.this, NormalTranslationActivity.class);
+                startActivity(normal);
+            }
+        });
+
+        Button fileTranslationButton = findViewById(R.id.action_file_translation);
+        fileTranslationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent file = new Intent(MainActivity.this, FileTranslationActivity.class);
+                startActivity(file);
+            }
+        });
+
+        Button historyButton = findViewById(R.id.action_history);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent history = new Intent(MainActivity.this, ActionHistory.class);
+                startActivity(history);
+            }
+        });
+
+        Button logoutButton = findViewById(R.id.action_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logout = new Intent(MainActivity.this, LoginActivity.class);
+                logout.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(logout);
+                finish();
+            }
+        });
+
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_normal_translation:
-                // Handle normal translation action
-                Intent normal = new Intent(this, NormalTranslationActivity.class);
-                startActivity(normal);
-                return true;
-            case R.id.action_file_translation:
-                // Handle file translation action
-                Intent file = new Intent(this, FileTranslationActivity.class);
-                startActivity(file);
-                return true;
-            case R.id.action_history:
-                // Handle history action
-                Intent history = new Intent(this, ActionHistory.class);
-                startActivity(history);
-                return true;
-            case R.id.action_logout:
-                // Handle logout action
-                // Clear user session data
-                // Navigate back to LoginActivity
-                Intent logout = new Intent(this, LoginActivity.class);
-                logout.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(logout);
-                finish();
-                return true;
-            case R.id.login:
-                // Start LoginActivity
-                Intent login = new Intent(this, LoginActivity.class);
-                startActivity(login);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
     }
 }
